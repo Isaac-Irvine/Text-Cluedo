8ee23adca0387a8072da9186657034a4e0702ae1
Tomay0 <thomaspage33@gmail.com>
2019-08-20 15:10:59 +1200

    Added window close confirmation popup

--- TextCluedo/src/Gui/Window.java
+++ TextCluedo/src/Gui/Window.java
@@ -2,18 +2,21 @@ package Gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 /**
  * Represents a window.
  * Made from a JFrame with a JPanel containing the whole
  */
-public class Window extends JPanel {
+public class Window extends JPanel implements WindowListener {
     private JFrame frame;
 
     /**
      * Create a window
-     * @param name Title of the JFrame
-     * @param width width of the window
+     *
+     * @param name   Title of the JFrame
+     * @param width  width of the window
      * @param height height of the window
      */
     public Window(String name, int width, int height) {
@@ -21,7 +24,8 @@ public class Window extends JPanel {
         frame.setSize(new Dimension(width, height));
         frame.setLayout(new BorderLayout());
         frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addWindowListener(this);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.setVisible(true);
 
         frame.add(this);
@@ -38,6 +42,7 @@ public class Window extends JPanel {
 
     /**
      * Width of the window
+     *
      * @return
      */
     public int getWidth() {
@@ -46,6 +51,7 @@ public class Window extends JPanel {
 
     /**
      * Height of the window
+     *
      * @return
      */
     public int getHeight() {
@@ -53,14 +59,17 @@ public class Window extends JPanel {
     }
 
     /**
-     * Close the window
+     * Ask for confirmation to close the window and close if confirmed
      */
     public void close() {
-        frame.dispose();
+        int option = JOptionPane.showConfirmDialog(this, "Are you sure you want to exit?", "Exit confirmation", JOptionPane.OK_CANCEL_OPTION);
+        if (option == 0)
+            frame.dispose();
     }
 
     /**
      * Change the title of the window
+     *
      * @param title
      */
     public void setTitle(String title) {
@@ -69,9 +78,75 @@ public class Window extends JPanel {
 
     /**
      * Set the menu bar
+     *
      * @param menuBar
      */
     public void setMenuBar(JMenuBar menuBar) {
         frame.setJMenuBar(menuBar);
+    }
+
+
+    /**
+     * When you try to close the window
+     *
+     * @param e
+     */
+    @Override
+    public void windowClosing(WindowEvent e) {
+        close();
+    }
+
+    /**
+     * Unused
+     *
+     * @param e
+     */
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
+
+    /**
+     * Unused
+     *
+     * @param e
+     */
+    @Override
+    public void windowOpened(WindowEvent e) {
+    }
+
+    /**
+     * Unused
+     *
+     * @param e
+     */
+    @Override
+    public void windowIconified(WindowEvent e) {
+    }
+
+    /**
+     * Unused
+     *
+     * @param e
+     */
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    /**
+     * Unused
+     *
+     * @param e
+     */
+    @Override
+    public void windowActivated(WindowEvent e) {
+    }
+
+    /**
+     * Unused
+     *
+     * @param e
+     */
+    @Override
+    public void windowDeactivated(WindowEvent e) {
     }
 }
